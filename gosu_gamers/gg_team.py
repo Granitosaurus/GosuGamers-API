cc68361d46f0f45f4c0295c36352f104b1c13efb
Bernard <bernardas@scrapinghub.com>
2015-01-04 15:59:37 +0200

    refactoring and port of team scrapers, only missing player scraping

--- gosu_gamers/gg_team.py
+++ gosu_gamers/gg_team.py
@@ -5,121 +5,132 @@ Package for retrieval of team ranks on http://www.gosugamers.net
 #local
 import re
 from gosu_gamers.team import Team
-from gosu_gamers.meta import GAMES
 #outer
 import requests
-import bs4
+from lxml import html
 
 __author__ = 'Bernard @ Bernardas.Alisauskas@gmail.com'
 
 
-class TeamScraper:
+class _TeamScraper:
     """Scraper for team ranks"""
-    def __init__(self, game):
-        if game not in GAMES:
-            print('game no in ')
-            raise AttributeError("parsed games must be one of: {}".format(', '.join(GAMES)))
-        self.game = game
-        self.teams = []
-
-    def make_dict(self):
-        """Returns list of dictionaries for json storage"""
-        return [team.make_dict(fill=True) for team in self.teams]
+    url = ''
+    game = ''
 
     def get_teams(self):
         """Fills up self.teams with team object with rank, name, country, score and rank_change if possible"""
-        request = requests.get('http://www.gosugamers.net/{game}/rankings#team'.format(game=self.game))
-        soup = bs4.BeautifulSoup(request.content)
-        total_pages = re.findall('=([0-9]+)', soup.find(text='Last').parent.parent['href'])[0]
+        request = requests.get(self.url)
+        tree = html.fromstring(request.content)
+        total_pages = re.findall('page=(\d)', ''.join(tree.xpath("//div[@class='pages']/a[contains(., 'Last')]/@href")))
+        total_pages = total_pages[0] if total_pages else 0
         total_pages = int(total_pages)
         for page in range(1, total_pages+1):
-            print('-getting team page: {}'.format(page))
-            request = requests.get('http://www.gosugamers.net/{game}/rankings?page={page}#team'
-                                   .format(game=self.game, page=page))
-            soup = bs4.BeautifulSoup(request.content)
-            table_rows = soup.find_all('tr', class_='ranking-link')
+            request = requests.get('{}{}{}'.format(self.url, '?page=', page))
+            tree = html.fromstring(request.content)
+            table_rows = tree.xpath("//tr[contains(@class, 'ranking-link')]")
             for tr in table_rows:
-                rank = ''
-                try:
-                    rank = tr.td.div.string
-                    country = tr.find('span', class_='main').span['title']
-                    name = tr.find('span', class_='main').text.strip()
-                    score = tr.find('td', class_='numbers').string
-                    rank_change = tr.find('td', class_='rank-change').i['title']
-                    team_id = tr['data-id']
-                    self.teams.append(Team(name, country, rank, score, rank_change, team_id))
-                except AttributeError:
-                    print('failed to get rank:', rank)
+                rank = ''.join(tr.xpath(".//td[@class='rank']/div/text()"))
+                country = ''.join(tr.xpath(".//span[contains(@class, 'flag')]/@title"))
+                name = ''.join(tr.xpath(".//h4/span/span/text()"))
+                score = ''.join(tr.xpath(".//td[@class='numbers']/text()"))
+                rank_change = ''.join(tr.xpath(".//td[@class='rank-change']/i/@title"))
+                team_id = ''.join(tr.xpath("./@data-id"))
+                yield Team(name, country, rank, score, rank_change, team_id, game=self.game)
 
-    def find_team(self, name=None, country=None, rank_exactly=None, score_exactly=None, rank_change=None, team_id=None,
-                  score_lower=None, score_higher=None, partial_name=None, rank_lower=None, rank_higher=None):
-        """
-        Finds a team by provided keyword attributes, returns a list of teams matching the filter settings
-        Keyword Arguments:
-        name - exact team name (case insensitive)
-        country - team origin country
-        rank_exactly - rank exactly
-        rank_lower - only teams with lower rank than provided
-        rank_higher - only teams with higher rank than provided
-        rank_change - the state of ank change
-        score_exactly - score exactly
-        score_lower - only teams with lower score than provided
-        score_higher - only teams with higher score than provided
-        team_id = exact team id
-        partial_name - part of name
-        """
-        if not self.teams:
-            raise AttributeError("Object team list is empty")
-        results = []
-        for team in self.teams:
-            all_args = []
-            if name is not None:
-                all_args.append(name.lower() == team.name.lower())
-            if country is not None:
-                all_args.append(country.lower() == team.country.lower())
-            if rank_exactly is not None:
-                all_args.append(rank_exactly == team.rank)
-            if rank_higher is not None:
-                if rank_higher == '':
-                    all_args.append(rank_higher == team.rank)
-                else:
-                    all_args.append(int(rank_higher) > int(team.rank))
-            if rank_lower is not None:
-                if rank_lower == '':
-                    all_args.append(rank_lower == team.rank)
-                else:
-                    all_args.append(int(rank_lower) < int(team.rank))
-            if score_exactly is not None:
-                all_args.append(score_exactly == team.score)
-            if rank_change is not None:
-                all_args.append(rank_change == team.rank_change)
-            if team_id is not None:
-                all_args.append(team_id == team.team_id)
-            if score_higher is not None:
-                if score_higher == '':
-                    all_args.append(score_higher == team.score)
-                else:
-                    all_args.append(int(score_higher) < int(team.score.replace(',', '')))
-            if score_lower is not None:
-                if score_lower == '':
-                    all_args.append(score_lower == team.score)
-                else:
-                    all_args.append(int(score_lower) > int(team.score.replace(',', '')))
-            if partial_name is not None:
-                if partial_name == '':
-                    all_args.append(partial_name == team.name)
-                else:
-                    all_args.append(partial_name.lower() in team.name.lower())
+    # def find_team(self, name=None, country=None, rank_exactly=None, score_exactly=None, rank_change=None, team_id=None,
+    #               score_lower=None, score_higher=None, partial_name=None, rank_lower=None, rank_higher=None):
+    #     """
+    #     Finds a team by provided keyword attributes, returns a list of teams matching the filter settings
+    #     Keyword Arguments:
+    #     name - exact team name (case insensitive)
+    #     country - team origin country
+    #     rank_exactly - rank exactly
+    #     rank_lower - only teams with lower rank than provided
+    #     rank_higher - only teams with higher rank than provided
+    #     rank_change - the state of ank change
+    #     score_exactly - score exactly
+    #     score_lower - only teams with lower score than provided
+    #     score_higher - only teams with higher score than provided
+    #     team_id = exact team id
+    #     partial_name - part of name
+    #     """
+    #     if not self.teams:
+    #         raise AttributeError("Object team list is empty")
+    #     results = []
+    #     for team in self.teams:
+    #         all_args = []
+    #         if name is not None:
+    #             all_args.append(name.lower() == team.name.lower())
+    #         if country is not None:
+    #             all_args.append(country.lower() == team.country.lower())
+    #         if rank_exactly is not None:
+    #             all_args.append(rank_exactly == team.rank)
+    #         if rank_higher is not None:
+    #             if rank_higher == '':
+    #                 all_args.append(rank_higher == team.rank)
+    #             else:
+    #                 all_args.append(int(rank_higher) > int(team.rank))
+    #         if rank_lower is not None:
+    #             if rank_lower == '':
+    #                 all_args.append(rank_lower == team.rank)
+    #             else:
+    #                 all_args.append(int(rank_lower) < int(team.rank))
+    #         if score_exactly is not None:
+    #             all_args.append(score_exactly == team.score)
+    #         if rank_change is not None:
+    #             all_args.append(rank_change == team.rank_change)
+    #         if team_id is not None:
+    #             all_args.append(team_id == team.team_id)
+    #         if score_higher is not None:
+    #             if score_higher == '':
+    #                 all_args.append(score_higher == team.score)
+    #             else:
+    #                 all_args.append(int(score_higher) < int(team.score.replace(',', '')))
+    #         if score_lower is not None:
+    #             if score_lower == '':
+    #                 all_args.append(score_lower == team.score)
+    #             else:
+    #                 all_args.append(int(score_lower) > int(team.score.replace(',', '')))
+    #         if partial_name is not None:
+    #             if partial_name == '':
+    #                 all_args.append(partial_name == team.name)
+    #             else:
+    #                 all_args.append(partial_name.lower() in team.name.lower())
+    #
+    #         if all(all_args):
+    #             results.append(team)
+    #     return results
 
-            if all(all_args):
-                results.append(team)
-        return results
+
+class CsGoTeamScraper(_TeamScraper):
+    url = 'http://www.gosugamers.net/counterstrike/rankings'
+    game = 'counterstrike'
+
+
+class Dota2TeamScraper(_TeamScraper):
+    url = 'http://www.gosugamers.net/dota2/rankings'
+    game = 'dota2'
+
+
+class LolTeamScraper(_TeamScraper):
+    url = 'http://www.gosugamers.net/lol/rankings'
+    game = 'lol'
+
+
+class HearthstoneTeamScraper(_TeamScraper):
+    url = 'http://www.gosugamers.net/hearthstone/rankings'
+    game = 'hearthstone'
+
+
+class HotsTeamScraper(_TeamScraper):
+    url = 'http://www.gosugamers.net/heroesofthestorm/rankings'
+    game = 'heroesofthestorm'
 
 
 if __name__ == '__main__':
     #testing
-    rc = TeamScraper('dota2')
-    rc.get_teams()
-    search = rc.find_team(rank_higher='')
-    for team in search:
-        print(team.name, team.rank)
+    dota_ts = Dota2TeamScraper()
+    dota_teams = list(dota_ts.get_teams())
+    example_team = dota_teams[1]
+    example_team.get_all_details()
+    print(example_team)
--- gosu_gamers/team.py
+++ gosu_gamers/team.py
@@ -3,10 +3,10 @@ Module for gosugamers.net team data storage and manipulation
 """
 import json
 import re
-import bs4
+from urllib.parse import quote, urljoin
+from lxml import html
 import requests
 #local
-from gosu_gamers.meta import DOMAIN
 from gosu_gamers.player import Player
 
 __author__ = 'Bernard @ Bernardas.Alisauskas@gmail.com'
@@ -14,116 +14,122 @@ __author__ = 'Bernard @ Bernardas.Alisauskas@gmail.com'
 
 class Team:
     """Storage class for team data"""
-    def __init__(self, name='', country='', rank='', score='', rank_change='', team_id='', url='', region_rank=''):
-        self.rank = rank
+    def __init__(self, name='', country='', rank='', score='', rank_change='',
+                 team_id='', url='', region_rank='', game='', social=None):
+        self.world_rank = rank
         self.region_rank = region_rank
         self.country = country
         self.name = name
-        self.score = score
+        self.rating = score
         self.rank_change = rank_change
         self.team_id = team_id
-        self.url = url
+        self.game = game
+        self.url = url or self.make_url()
         self.team_members = []
         self.manager = None
-        self.description = ''
+        self.social = social
+
+        self.match_history_url = urljoin(self.url, 'matches')
+        self.player_history_url = urljoin(self.url, 'player-history')
+
+    def make_url(self):
+        """Makes url to the team's page from team_id and team_name"""
+        team_name = quote(self.name)
+        return 'http://www.gosugamers.net/{}/teams/{}-{}'.format(self.game, self.team_id, team_name)
 
     def check_data(self):
         """Checks if all object data is filled in"""
-        return all([self.rank, self.region_rank, self.country, self.name, self.score, self.rank_change, self.team_id,
-                    self.url, self.team_members, self.manager, self.description])
+        return all([self.world_rank, self.region_rank, self.country, self.name, self.rating, self.rank_change, self.team_id,
+                    self.url, self.team_members, self.manager])
 
     def get_all_details(self):
         """Fills up details if Team object has an ID does not fill up self.rank_change"""
-        if not self.url:
-            self.get_url()
         request = requests.get(self.url)
-        soup = bs4.BeautifulSoup(request.content)
-        try:
-            if not self.description:
-                self.description = soup.find('div', id='wiki-content').p.string
-        except AttributeError:
-            self.description = ''
-        if not self.score:
-            self.score = soup.find('span', class_='tooltip').string
-        if not self.rank:
-            self.rank = soup.find_all('div', class_='ranking')[0].span.string
+        tree = html.fromstring(request.content)
+        if not self.rating:
+            self.rating = ''.join(tree.xpath('//span[@class="tooltip"]/text()'))
+        if not self.world_rank:
+            self.world_rank = ''.join(tree.xpath("//div[@class='ranking'][1]/span/text()"))
         if not self.region_rank:
-            self.region_rank = soup.find_all('div', class_='ranking')[0].span.string
+            self.region_rank = ''.join(tree.xpath("//div[@class='ranking'][2]/span/text()"))
         if not self.name:
-            self.name = soup.find('div', id='profile-menu').div.h3.string
+            self.name = ''.join(tree.xpath("//div[@class='teamNameHolder']/h1/text()")).rsplit('-', 1)[0]
         if not self.country:
-            self.country = soup.find('span', class_='flag')['title']
+            self.country = ''.join(tree.xpath("//div[@class='teamCountry']/span[contains(@class, 'flag')]/@title"))
+        if not self.social:
+            self.social = tree.xpath("//div[@class='social']/a/@href")
         #Players
-        if not self.team_members:
-            players = soup.find_all('a', class_='player')
-            for player in players:
-                nickname = player.h5.string
-                fullname = player.find_all('span')[1].string
-                if player.find('span', class_='manager'):
-                    manager = True
-                else:
-                    manager = False
-                photo_url = DOMAIN[:-1] + re.findall("\('(.*?)'\)", player.div.attrs['style'])[0]
-                famous_heroes = [item['alt'] for item in player.find('div', class_='heroes').find_all('img')]
-                url = DOMAIN + player['href']
-                if not manager:
-                    self.team_members.append(Player(nickname, fullname, photo_url, famous_heroes, url))
-                else:
-                    self.manager = Player(nickname, fullname, photo_url, famous_heroes, url)
+        # if not self.team_members:
+        #     players = tree.find_all('a', class_='player')
+        #     for player in players:
+        #         nickname = player.h5.string
+        #         fullname = player.find_all('span')[1].string
+        #         if player.find('span', class_='manager'):
+        #             manager = True
+        #         else:
+        #             manager = False
+        #         photo_url = DOMAIN[:-1] + re.findall("\('(.*?)'\)", player.div.attrs['style'])[0]
+        #         famous_heroes = [item['alt'] for item in player.find('div', class_='heroes').find_all('img')]
+        #         url = DOMAIN + player['href']
+        #         if not manager:
+        #             self.team_members.append(Player(nickname, fullname, photo_url, famous_heroes, url))
+        #         else:
+        #             self.manager = Player(nickname, fullname, photo_url, famous_heroes, url)
 
-    def make_dict(self, fill=False):
+    def __dict__(self, fill=False):
         """
         Returns a formatted unordered dictionary of all team data
         Keyword Arguments:
         fill - if true will try to fill in every element of the Team object before outputting dict. [Default = False]
         """
-        if fill:
-            self.get_all_details()
-        try:
-            manager = self.manager.make_dict()
-        except AttributeError:
-            manager = ''
-        print('writing json for: #{} {}'.format(self.rank, self.name))
         data = {
             'meta': {
                 'team id': self.team_id,
-                'url': self.url
+                'url': self.url,
+                'match_history_url': self.match_history_url,
+                'player_history_url': self.player_history_url,
             },
             'name': self.name,
-            'rank': self.rank,
+            'game': self.game,
+            'rank': self.world_rank,
             'region rank': self.region_rank,
             'country': self.country,
-            'manager': manager,
-            'desciption': self.description,
+            'manager': self.manager,
             'rank change': self.rank_change,
-            'score': self.score,
+            'region_rank': self.region_rank,
+            'world_rank': self.world_rank,
+            'rating': self.rating,
+            'social': self.social,
             'team members': [p.make_dict() for p in self.team_members]
         }
         return data
 
-    def get_url(self, team_id=''):
-        """Finds an url to the team's profile from team id"""
-        if not team_id:
-            if not self.team_id:
-                raise AttributeError("team_id not set")
-            else:
-                team_id = self.team_id
-
-        if not self.url:
-            request = requests.get('http://www.gosugamers.net/rankings/show/team/{team_id}'.format(team_id=team_id))
-            soup = bs4.BeautifulSoup(request.content)
-            url = ''
-            try:
-                url = DOMAIN + soup.find('div', class_='base').h3.a['href']
-            except AttributeError:
-                print("Invalid team_id")
-            self.url = url
-            return self.url
+    # def get_url(self, team_id=''):
+    #     """Finds an url to the team's profile from team id"""
+    #     if not team_id:
+    #         if not self.team_id:
+    #             raise AttributeError("team_id not set")
+    #         else:
+    #             team_id = self.team_id
+    #
+    #     if not self.url:
+    #         request = requests.get('http://www.gosugamers.net/rankings/show/team/{team_id}'.format(team_id=team_id))
+    #         soup = bs4.BeautifulSoup(request.content)
+    #         url = ''
+    #         try:
+    #             url = DOMAIN + soup.find('div', class_='base').h3.a['href']
+    #         except AttributeError:
+    #             print("Invalid team_id")
+    #         self.url = url
+    #         return self.url
 
     def __str__(self):
-        return '{rank} {country} {name} {score} {rank_change} {team_id}'\
-            .format(rank=self.rank, country=self.country, name=self.name, score=self.score,
-                    rank_change=self.rank_change, team_id=self.team_id)
+        return 'game: {game}\nrank: {rank}\nregion rank: {rrank}\ncountry: {country}\nname: {name}\nscore: {score}\nrank change: {rank_change}\n' \
+               'team_id: {team_id}\nsocial: {social}\nmatch history: {mhistory}\nplayer history: {phistory}\n' \
+               ''\
+            .format(rank=self.world_rank, country=self.country, name=self.name, score=self.rating,
+                    rank_change=self.rank_change, team_id=self.team_id, game=self.game, social=self.social,
+                    mhistory=self.match_history_url, phistory=self.player_history_url, rrank=self.region_rank)
 
 if __name__ == '__main__':
     team = Team(team_id='4387')
@@ -131,7 +137,7 @@ if __name__ == '__main__':
     for member in team.team_members:
         print(member)
 
-    json_data = team.make_dict()
+    json_data = team.__dict__()
     json_data = json.dumps(json_data)
     print(json_data)
     print(team)
