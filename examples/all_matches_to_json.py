6851a9738bdf260ee8e257ad92e90a772f85bdc3
Bernard <bernardas@scrapinghub.com>
2015-02-19 15:46:53 +0200

    example for all matches to json

--- examples/all_matches_to_json.py
+++ examples/all_matches_to_json.py
@@ -6,18 +6,20 @@ import json
 from gosu_gamers import gg_match
 
 
-def all_games_to_json():
+def all_games_to_json(get_streams=True):
     ggms = gg_match.MatchScraper()
 
     live = ggms.find_live_matches()
     upcoming = ggms.find_upcoming_matches()
     recent = ggms.find_recent_matches()
-    games_dict = {'live': [m.__dict__() for m in live],
-                  'upcoming': [[m.__dict__() for m in upcoming]],
-                  'recent': [[m.__dict__() for m in recent]]}
+    games_dict = {'live': [m.__dict__(get_streams=get_streams) for m in live],
+                  'upcoming': [[m.__dict__(get_streams=get_streams) for m in upcoming]],
+                  'recent': [[m.__dict__(get_streams=get_streams) for m in recent]]}
 
     date = datetime.now().strftime('%Y%m%d%H%M%S')
-    with open('all_matches_{}.json'.format(date), 'w') as json_file:
+    filename = 'all_matches_{}.json'.format(date)
+    with open(filename, 'w') as json_file:
+        print('Saving to "{}"'.format(filename))
         json_file.write(json.dumps(games_dict))
 
 if __name__ == '__main__':
