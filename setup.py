4d52f52bcea0e3a899e357a39e02dfe4bbedebb7
Bernard <bernardas@scrapinghub.com>
2015-02-19 09:55:08 +0200

    fixed an issue where setup would fail to unpack .txt files

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name='GosuGamers-API',
     version='0.2',
     packages=['gosu_gamers', 'gosu_gamers.data', 'gosu_gamers.utils'],
+    package_data={'': ['*.txt'],
+                  'data': ['*.txt']},
     url='https://github.com/Granitas/GosuGamers-API/',
     license='GNU GENERAL PUBLIC LICENSE',
     author='https://github.com/Granitas',
